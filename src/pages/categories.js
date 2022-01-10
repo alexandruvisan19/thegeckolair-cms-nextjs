04703428282324a76af1e3dcd4e3e30d6f805b77
Alexandru Visan <alexandruvisan1908@gmail.com>
2022-01-10 15:37:35 +0200

    remove console log

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -42,7 +42,6 @@ export default function Categories({ categories }) {
           <SectionTitle>All Categories</SectionTitle>
           <ul className={styles.categories}>
             {categories.map((category) => {
-              console.log(categories);
               return (
                 category.count && (
                   <li key={category.slug}>
